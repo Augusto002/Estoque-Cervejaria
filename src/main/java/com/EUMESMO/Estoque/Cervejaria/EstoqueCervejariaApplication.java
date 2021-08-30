2e5d697b54517274e9dd412e8ddb5ea93ef3e609
Augusto002 <henrique.augustob27@gmail.com>
2021-08-30 09:36:53 -0300

    Enviando os arquivos para o GIT

---
+++ src/main/java/com/EUMESMO/Estoque/Cervejaria/EstoqueCervejariaApplication.java
@@ -0,0 +1,13 @@
+package com.EUMESMO.Estoque.Cervejaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueCervejariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueCervejariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/EUMESMO/Estoque/Cervejaria/EstoqueCervejariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EUMESMO.Estoque.Cervejaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueCervejariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
